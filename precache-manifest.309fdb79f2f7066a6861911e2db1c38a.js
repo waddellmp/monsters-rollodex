1d8de5efc4b950807b68ba120475c2eae409e360
Matthew Waddell <matthew.waddell@swaymedical.com>
2020-03-09 14:02:05 -0500

    Updates

---
+++ precache-manifest.309fdb79f2f7066a6861911e2db1c38a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10539efa32f9ec97c9d379329fbe6e16",
+    "url": "/monsters-rollodex/index.html"
+  },
+  {
+    "revision": "1fece59a77dac0834166",
+    "url": "/monsters-rollodex/static/css/main.8fe4654a.chunk.css"
+  },
+  {
+    "revision": "cd9a330a13a0d3a119a8",
+    "url": "/monsters-rollodex/static/js/2.a3840bda.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rollodex/static/js/2.a3840bda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fece59a77dac0834166",
+    "url": "/monsters-rollodex/static/js/main.41092919.chunk.js"
+  },
+  {
+    "revision": "9f790c81f22e55f646ae",
+    "url": "/monsters-rollodex/static/js/runtime-main.1c78d067.js"
+  }
+]);
